045cc4122446c10b7fe486bb3617894d98ba5183
DuanPretorius07 <duan@methoddata.com>
2026-01-12 11:55:46 +0200

    Fix Vercel serverless entry to dynamically import ESM server

--- api/serverless.js
+++ api/serverless.js
@@ -1,15 +1,18 @@
 // Vercel serverless function entry point
-// This exports the Express app for Vercel's serverless environment
+// Keep this file CommonJS-friendly so Vercel can require it without ESM errors.
 
-// Vercel automatically sets VERCEL=1, but we should mark it explicitly for our code
 if (!process.env.VERCEL) {
   process.env.VERCEL = '1';
 }
 
-// Note: Vercel provides environment variables directly, no need to load .env
-// Import the Express app - env vars should be available at import time in Vercel
-import app from '../server/dist/server.js';
+let cachedAppPromise;
 
-// Vercel serverless functions expect a default export that handles (req, res)
-// Since we're exporting the Express app directly, Vercel will use it as a request handler
-export default app;
+module.exports = async (req, res) => {
+  // Lazily import the ESM Express app. Dynamic import works in CJS on Vercel.
+  if (!cachedAppPromise) {
+    cachedAppPromise = import('../server/dist/server.js').then(mod => mod.default);
+  }
+
+  const app = await cachedAppPromise;
+  return app(req, res);
+};
